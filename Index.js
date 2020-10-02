280e647f01e6181e222eaf10a0bd4d0bd914a7b2
Seymore Biggins <zachharris024@gmail.com>
2020-10-02 13:28:39 -0500

    added file structure and node packages

---
+++ Index.js
@@ -0,0 +1,9 @@
+// __tests__/            // jest tests
+//   Employee.test.js
+//   Engineer.test.js
+//   Intern.test.js
+//   Manager.test.js
+// dist/               // rendered output (HTML) and CSS style sheet
+// lib/                // classes
+// src/                // template helper code
+// Index.js            // runs the application
